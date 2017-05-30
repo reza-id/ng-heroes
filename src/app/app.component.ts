af18e3e352962634ad7b28154442472652f1df7f
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-30 08:40:19 +0700

    Hide the empty detail with ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,9 +29,11 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div><label>name: </label><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div><label>name: </label><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
+    </div>
   `,
   styles: [`
     .selected {
