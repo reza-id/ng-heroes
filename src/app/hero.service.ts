fac84c5914f31c8103178c9ffa702b3457639222
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-30 14:24:12 +0700

    Add the ability to add heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,6 +33,14 @@ export class HeroService {
                .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this._http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this._http
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -19,6 +19,17 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
