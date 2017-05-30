6443feb07fed0fd501af6bae5d96b5098e6ceab6
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-30 14:08:38 +0700

    npm install --save angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpModule }    from '@angular/http';
 
 import { HeroService } from './hero.service';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }  from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }  from './heroes.component';
@@ -17,6 +21,7 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
