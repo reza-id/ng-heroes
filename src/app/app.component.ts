644d63a3c3612badb07ecc3f3a5ea6a43cb516e5
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-30 08:39:14 +0700

    Selecting a hero with event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,13 +25,13 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div><label>name: </label><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
   `,
   styles: [`
     .selected {
@@ -86,8 +86,9 @@ const HEROES: Hero[] = [
 export class AppComponent  {
   title = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
