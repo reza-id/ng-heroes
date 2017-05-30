cd633bb2c354a37574df9eac7f17a51233257910
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-30 13:50:01 +0700

    prepare http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { HeroService } from './hero.service';
 
@@ -16,6 +16,7 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
